69b11c7c0fbb54de80a1ae35ca0072c1f2f18add
DevAbabil <ababilhossain35@gmail.com>
2024-11-06 00:12:32 +0600

    update domget v1.js, display error boundary only in development mode

--- dist/js/v1.js
+++ dist/js/v1.js
@@ -41,61 +41,66 @@ var $ = (qSelector) => {
 };
 
 function displayError(message) {
-  $("#DOMgetError").style.display = "flex";
-  $(".msg-content").innerText = message;
-  $("body").style.overflow = "hidden";
+  if (window.location.hostname === atob("MTI3LjAuMC4x") || window.location.hostname === atob("bG9jYWxob3N0")) {
+    $("#DOMgetError").style.display = "flex";
+    $(".msg-content").innerText = message;
+    $("body").style.overflow = "hidden";
+  }
   console.error(message);
 }
 
-(() => {
-  const msg = document.createElement("div");
-  msg.setAttribute("id", "DOMgetError");
-  msg.innerHTML = `
-    <div class="msg-contentainer">
-      <div class="msg-title">Error :</div>
-      <div class="msg-content"></div>
-      <div class="console">See the console for more details.</div>
-    </div>
-  `;
-  const style = document.createElement("style");
-  style.setAttribute("style-for", "domget");
-  style.innerHTML = ` 
-    #DOMgetError {
-      width: 100%;
-      height: 100vh;
-      position: fixed;
-      top: 0;
-      left: 0;
-      justify-content: center;
-      align-items: center;
-      background: rgba(245, 222, 179, 0.514);
-      display: none;
-    }
-    #DOMgetError .msg-contentainer {
-      width: 390px;
-      padding: 20px;
-      border: 1px solid rgba(128, 128, 128, 0.568);
-      box-shadow: 0px 0px 4px black inset;
-      border-radius: 8px;
-      display: flex;
-      flex-direction: column;
-      row-gap: 5px;
-      background: white;
-    }
-    #DOMgetError .msg-contentainer .msg-title {
-      color: red;
-      font-weight: bold;
-      font-size: 1.25rem;
-      line-height: 1.75rem;
-    }
-    #DOMgetError .msg-contentainer .msg-content {
-      color: gray;
-      font-weight: bold;
-    }
-    #DOMgetError .msg-contentainer .console {
-      color: rgb(66, 66, 66);
-    }
- `;
-  $("head").appendChild(style);
-  $("body").appendChild(msg);
-})();
+window.location.hostname === atob("MTI3LjAuMC4x") || window.location.hostname === atob("bG9jYWxob3N0")
+  ? (() => {
+      const msg = document.createElement("div");
+      msg.setAttribute("id", "DOMgetError");
+      msg.innerHTML = `
+      <div class="msg-contentainer">
+        <div class="msg-title">Error :</div>
+        <div class="msg-content"></div>
+        <div class="console">See the console for more details.</div>
+      </div>
+    `;
+      const style = document.createElement("style");
+      style.setAttribute("style-for", "domget");
+      style.innerHTML = ` 
+      #DOMgetError {
+        width: 100%;
+        height: 100vh;
+        position: fixed;
+        top: 0;
+        left: 0;
+        justify-content: center;
+        align-items: center;
+        background: rgba(245, 222, 179, 0.514);
+        display: none;
+      }
+      #DOMgetError .msg-contentainer {
+        width: 390px;
+        padding: 20px;
+        border: 1px solid rgba(128, 128, 128, 0.568);
+        box-shadow: 0px 0px 4px black inset;
+        border-radius: 8px;
+        display: flex;
+        flex-direction: column;
+        row-gap: 5px;
+        background: white;
+      }
+      #DOMgetError .msg-contentainer .msg-title {
+        color: red;
+        font-weight: bold;
+        font-size: 1.25rem;
+        line-height: 1.75rem;
+      }
+      #DOMgetError .msg-contentainer .msg-content {
+        color: gray;
+        font-weight: bold;
+      }
+      #DOMgetError .msg-contentainer .console {
+        color: rgb(66, 66, 66);
+      }
+    `;
+
+      $("head").appendChild(style);
+      $("body").appendChild(msg);
+    })()
+  : null;
