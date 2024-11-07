c8292a9af02421ddb2b612c5f3db4c524dc159a9
DevAbabil <ababilhossain35@gmail.com>
2024-11-07 06:35:19 +0600

    make all content dynamic

---
+++ dist/js/commonConfig.js
@@ -0,0 +1,39 @@
+// initiate highlight js
+hljs.highlightAll();
+
+function setState(callback) {
+  callback();
+  renderDOM();
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  const id = setTimeout(() => {
+    $("#loader").remove();
+    $("html").classList.remove("overflow-hidden");
+    $("body").classList.remove("overflow-hidden");
+    clearTimeout(id);
+  }, 300);
+});
+
+const toast = (message) => {
+  Toastify({
+    text: message,
+    duration: 3200,
+    newWindow: true,
+    close: true,
+    gravity: "top",
+    position: "center",
+    stopOnFocus: true,
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+  }).showToast();
+};
+
+const copyToClipBoard = (value, context = "") => {
+  const tempInput = document.createElement("textarea");
+  tempInput.value = value;
+  tempInput.select();
+  navigator.clipboard.writeText(tempInput.value);
+  toast(`Succesfully copied ${context}`);
+};
